be85ec9f2ffc22e9ca2c5a6915c844c96884c635
jeff-lund <jdl3@pdx.edu>
2020-01-22 18:56:22 -0800

    Power of leap years has been unlocked

--- exampleProject/src/main/java/edu/pdx/cs410J/jdl3/LeapYear.java
+++ exampleProject/src/main/java/edu/pdx/cs410J/jdl3/LeapYear.java
@@ -4,4 +4,17 @@ public class LeapYear {
     public static void main(String[] args) {
         System.exit(0);
     }
+
+    public static boolean isLeapYear(int year) throws IllegalArgumentException {
+        if (year < 1) {
+            throw new IllegalArgumentException("Year must be positive");
+        }
+        if(year % 400 == 0){
+            return true;
+        } else if (year % 100 == 0) {
+            return false;
+        } else {
+            return year % 4 == 0;
+        }
+    }
 }
--- exampleProject/src/test/java/edu/pdx/cs410J/jdl3/LeapYearTest.java
+++ exampleProject/src/test/java/edu/pdx/cs410J/jdl3/LeapYearTest.java
@@ -6,5 +6,35 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class LeapYearTest {
     @Test
-    public void pass() {}
+    public void yearIsDivisibleBy400IsLeapYear(){
+        assert(LeapYear.isLeapYear(2000));
+    }
+
+    @Test
+    public void yearIsDivisibleBy100AndNot400IsNotLearYear() {
+        assertFalse(LeapYear.isLeapYear(1700));
+    }
+
+    @Test
+    public void yearIsDivisibleBy4ButNot100IsLeapYear(){
+        assert(LeapYear.isLeapYear(2008));
+    }
+    @Test
+    public void yearNotDivisibleByFour() {
+        assertFalse(LeapYear.isLeapYear(2019));
+    }
+    @Test
+    public void yearNotDivisibleByFourButIsDivisibleBy100() {
+        assertTrue(LeapYear.isLeapYear(2016));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void negativeYearThrowsException() {
+        LeapYear.isLeapYear(-1000);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void zeroYearThrowsException() {
+        LeapYear.isLeapYear(0);
+    }
+
 }
